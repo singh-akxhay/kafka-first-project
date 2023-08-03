0dcd9b0df674ee56580f861d294f7f5969f6a14a
Akshay Singh <singh.ak2104@gmail.com>
2023-08-03 20:56:59 +0530

    Init commit

---
+++ src/main/kotlin/com/singhakxhay/kafkafirstproject/KafkaFirstProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.singhakxhay.kafkafirstproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaFirstProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaFirstProjectApplication>(*args)
+}
