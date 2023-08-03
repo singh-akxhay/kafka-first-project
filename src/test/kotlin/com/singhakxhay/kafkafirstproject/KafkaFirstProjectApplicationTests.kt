a556706fe6d487e80e3a270dadc23c7843330fcb
Akshay Singh <singh.ak2104@gmail.com>
2023-08-03 20:59:50 +0530

    Added configs

---
+++ src/test/kotlin/com/singhakxhay/kafkafirstproject/KafkaFirstProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.singhakxhay.kafkafirstproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaFirstProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
